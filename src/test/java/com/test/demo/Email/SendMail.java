1d7493b82421a4fddd48b8f48c623ba5bfa7ce7a
sk1298 <shantam23khare@gmail.com>
2016-05-12 17:01:58 +0530

    Zip attachment code added.
    Change-Id: I0000000000000000000000000000000000000002
    Signed-off-by: sk1298

--- src/test/java/com/test/demo/Email/SendMail.java
+++ src/test/java/com/test/demo/Email/SendMail.java
@@ -2,6 +2,9 @@ package com.test.demo.Email;
 
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
@@ -10,18 +13,19 @@ import javax.mail.internet.MimeMultipart;
 
 public class SendMail {
 	public static void main(String args[]) throws MessagingException {
-		String recipients[] = {"jaidip1994@gmail.com","shantam23khare@gmail.com"}; 
+		String recipients[] = { "jaidip1994@gmail.com", "shantam23khare@gmail.com" };
 		boolean debug = false;
 		Properties props = new Properties();
-		//Mail Prop
-		props.put("mail.transport.protocol", "smtps");
+		// Mail Prop
+		props.put("mail.transport.protocol", "smtp");
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.user", "sbsdbdj@gmail.com");
 		props.put("mail.smtp.password", "sbsdbj2ee");
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
-		props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.port", "25");
+		// props.put("mail.smtp.socketFactory.class",
+		// "javax.net.ssl.SSLSocketFactory");
 
 		Authenticator auth = (new SendMail()).new SMTPAuthenticator();
 		Session session1 = Session.getDefaultInstance(props, auth);
@@ -40,40 +44,43 @@ public class SendMail {
 		String filepath = AppZip.start();
 		String filename = filepath.substring(filepath.lastIndexOf("\\") + 1);
 		BodyPart messageBodyPart = new MimeBodyPart();
-		messageBodyPart
-				.setContent(
-						"Hi,<br/>This is an automatically generated mail.<br/>To render the screenshots in the reports, the folders should be extracted and stored in the following directories.<br/>"
-								+ AppZip.SOURCE_FOLDERSDEMO +"<br/><p>The Report is in this location :- <a href=" + AppZip.FINAL_REPORT_LOCATION
-								+ ">" + AppZip.FINAL_REPORT_LOCATION + "</a>. The File name is " + filename
-								+ ".</p><br/><br/><h3>Summary of Reports:-</h3>" + AppZip.MessageBody + "<br/><br/>Regards,<br/>Auto-Bot",
-						"text/html");
+		messageBodyPart.setContent(
+				"Hi,<br/>This is an automatically generated mail.<br/>To render the screenshots in the reports, the folders should be extracted and stored in the following directories.<br/>"
+						+ AppZip.SOURCE_FOLDERSDEMO + "<br/><p>The Report is in this location :- <a href="
+						+ AppZip.FINAL_REPORT_LOCATION + ">" + AppZip.FINAL_REPORT_LOCATION + "</a>. The File name is "
+						+ filename + ".</p><br/><br/><h3>Summary of Reports:-</h3>" + AppZip.MessageBody
+						+ "<br/><br/>Regards,<br/>Auto-Bot",
+				"text/html");
 		Multipart mpart = new MimeMultipart();
 		mpart.addBodyPart(messageBodyPart);
 		// This part basically is used for sending the attachment to recipient.
-		/*
-		 * messageBodyPart = new MimeBodyPart(); DataSource source = new
-		 * FileDataSource(filepath); messageBodyPart.setDataHandler(new
-		 * DataHandler(source)); messageBodyPart.setFileName(filename);
-		 * mpart.addBodyPart(messageBodyPart);
-		 */
+
+		messageBodyPart = new MimeBodyPart();
+		DataSource source = new FileDataSource(filepath);
+		messageBodyPart.setDataHandler(new DataHandler(source));
+		messageBodyPart.setFileName(filename);
+		mpart.addBodyPart(messageBodyPart);
+
 		msg.setContent(mpart);
 		msg.saveChanges();
 
 		Transport transport = session1.getTransport("smtp");
 		transport.connect("smtp.gmail.com", "sbsdbdj@gmail.com", "sbsdbj2ee");
 		System.out.println("Message sending is in progress");
-		transport.sendMessage(msg, msg.getAllRecipients());
+
+		// transport.sendMessage(msg, msg.getAllRecipients());
+
 		transport.close();
-		// Transport.send(msg);
+		Transport.send(msg);
 		System.out.println("Message Sent Successfully");
 	}
 
 	private class SMTPAuthenticator extends javax.mail.Authenticator {
 		public PasswordAuthentication getPasswordAuthentication() {
 
-			String user="sbsdbdj@gmail.com";
-			String pwd= "sbsdbj2ee";
-			return new PasswordAuthentication(user,pwd);
+			String user = "sbsdbdj@gmail.com";
+			String pwd = "sbsdbj2ee";
+			return new PasswordAuthentication(user, pwd);
 		}
 	}
 
